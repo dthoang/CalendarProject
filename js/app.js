6713fcd66e8aebfcaebd70f2dc5b25d3100aad7e
daniel <dthoang@uw.edu>
2015-11-29 19:32:02 -0800

    First commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$('#calendar').fullCalendar({
+    defaultView: 'timelineMonth',
+    events: [
+        // events go here
+    ],
+    resources: [
+        // resources go here
+    ]
+    // other options go here...
+});
